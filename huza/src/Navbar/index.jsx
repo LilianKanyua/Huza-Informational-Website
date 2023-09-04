b3c31b13b9eee1e54ca942d1459116c4cf4ae183
LilianKanyua <kanyualilianm@gmail.com>
2023-09-04 15:06:06 +0300

    created home feature

--- huza/src/Navbar/index.jsx
+++ huza/src/Navbar/index.jsx
@@ -7,7 +7,7 @@ const Navbar =()=>{
             <img src={process.env.PUBLIC_URL +'/images/Logo.png'} alt="Pic" className="logo"      />
             <ul className="list">
                 <li>Home</li>
-                <li>AboutUs</li>
+                <li>About</li>
                 <li>Product</li>
             </ul>
  </div>
@@ -17,11 +17,6 @@ const Navbar =()=>{
             </br>
              to unlimited job opportunities </p>
             <h3>Access our services right from your feature phone with no internet needed</h3>
- </div>
- <div className="images">
- <img src={process.env.PUBLIC_URL +'/images/Group 6.png'} alt="icons" className="icons-icons"/>
- <img src={process.env.PUBLIC_URL +'/images/phon.png'} alt="icons" className="phone-icon"/>
- <hr className="hr"></hr>
  </div>
  </div>
     )
